46f3ca22c4ff08af0dabd6d091564e2e5e321938
akahito1006 <nekodora5k@gmail.com>
2021-02-20 16:35:19 +0900

    [skillup] 4-3 javascript while, if, var

---
+++ javascript/4-1,2/app.js
@@ -0,0 +1,8 @@
+var int1 = 2;
+var int2 = 3;
+
+alert('足し算の結果は・・・' + (int1 + int2))
+alert('引き算の結果は・・・' + (int1 - int2))
+alert('掛け算の結果は・・・' + (int1 * int2))
+alert('割り算の結果は・・・' + (int1 / int2))
+alert('割り算の余りは・・・' + (int1 % int2))
