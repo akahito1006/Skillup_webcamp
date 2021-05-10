8391b337ef9199e3a70b0a65382297af276abd24
akahito1006 <nekodora5k@gmail.com>
2021-05-10 20:21:30 +0900

    JS基本文法

---
+++ JS_basics/js/index.js
@@ -0,0 +1,65 @@
+// -------------1.変数と定数-------------
+// 変数
+let let_word = 'Hello World';
+let_word = 'Hello World2';
+
+// 定数
+const const_word = 'Hi world!!!';
+
+// -------------2.配列-------------
+let weekdays = ['mon', 'tue', 'wed', 'thu', 'fri'];
+// const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri']
+// console.log(weekdays[0]);
+
+// -------------3.ループ文-------------
+// let index = 0;
+// while (index < weekdays.length) {
+//   // 繰り返したい命令
+//   console.log(weekdays[index])
+//   index++; //index = 5
+// }
+
+// if/else
+if(weekdays.length > 5){
+  console.log('土日はまだ？');
+} else {
+  console.log('お疲れ！')
+};
+
+// -------------4.関数-------------
+const test = (arg) => { //arg;引数
+  // const test = function() {
+  // ここに実行したい処理を書く
+    if(weekdays.length > arg){
+      console.log('土日はまだ？');
+    } else {
+      console.log('お疲れ！')
+    }
+};
+// test(3);
+
+// -------------5.オブジェクト-------------
+const phone = {
+  // key: value;
+  color: 'white',
+  size: 'handful',
+  stars: '4 of 5',
+  // 関数
+  ring: () => {
+    console.log('ring!');
+  }
+};
+// console.log(phone.ring());
+
+// -------------6.あらかじめあるオブジェクト-------------
+// 1.window
+// window.alert('Hello!!!');
+
+// 2.document
+// console.log(document.getElementsByTagName('p')[0]);
+
+// 3.event
+document.getElementsByTagName('p')[0].addEventListener('click', () => {
+  // 命令を書く
+  window.alert('Hello World!')
+});
